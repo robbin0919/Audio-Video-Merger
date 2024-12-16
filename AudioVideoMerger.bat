1862a82efcee596468da68e14281b672dc6ac872
robbin lee <66932401+robbin0919@users.noreply.github.com>
2024-12-16 23:32:54 +0800

    Update AudioVideoMerger.bat

--- AudioVideoMerger.bat
+++ AudioVideoMerger.bat
@@ -12,3 +12,16 @@ echo [INFO] Welcome to use the batch script to merge mp4 and m4a files
 echo [INFO] Author: @robbin019
 echo [INFO] Script version: 0.1
 echo.
+if not exist ffmpeg.exe (
+    set extracted_text=Not Available
+) else (
+    for /f "delims=" %%i in ('ffmpeg -version 2^>^&1') do (
+        set "ffmpeg_version=%%i"
+        goto :done
+       )
+    :done
+    for /f "tokens=2,3" %%a in ("!ffmpeg_version!") do (
+        set "extracted_text=%%a %%b"
+      )
+       )
+echo !extracted_text!
