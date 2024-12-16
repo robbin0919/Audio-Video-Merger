049ea9cb6b694d87a3590bc68f4626804e3742fa
robbin lee <66932401+robbin0919@users.noreply.github.com>
2024-12-16 23:20:39 +0800

    Update AudioVideoMerger.bat

--- AudioVideoMerger.bat
+++ AudioVideoMerger.bat
@@ -1,1 +1,14 @@
+@ECHO OFF
+setlocal enabledelayedexpansion
 
+title Audio and Video Merger by @robbin019
+
+set "input_folder=.\input"
+set "temp_folder=temp"
+set "output_folder=output"
+rmdir %temp_folder% /s /q >nul 2>nul
+echo.
+echo [INFO] Welcome to use the batch script to merge mp4 and m4a files
+echo [INFO] Author: @robbin019
+echo [INFO] Script version: 0.1
+echo.
