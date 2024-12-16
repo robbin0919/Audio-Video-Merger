8e852f2b4aeb99e2280d54418f86298c97900241
robbin lee <66932401+robbin0919@users.noreply.github.com>
2024-12-16 23:40:03 +0800

    Update AudioVideoMerger.bat

--- AudioVideoMerger.bat
+++ AudioVideoMerger.bat
@@ -25,3 +25,57 @@ if not exist ffmpeg.exe (
       )
        )
 echo !extracted_text!
+
+echo [INFO] Dependence ffmpeg %extracted_text%
+echo [INFO] You can download or update ffmpeg version from ffmpeg.org/download.html
+echo [INFO] You also can just delete ffmpeg.exe and run this script again to try to download latest version
+echo.
+echo [NOTICE] Data no price, caution proceed
+echo.
+echo [*] Press any key to continue
+echo.
+pause >nul
+
+if not exist ffmpeg.exe (
+    echo [INFO] Dependence ffmpeg.exe not found
+    echo  start download
+    echo.
+    pause >nul
+    mkdir !temp_folder! >nul 2>nul
+    title Downloading ffmpeg.exe ^| Audio and Video Merger  
+    echo [INFO] Downloading ffmpeg.exe
+    curl -L https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip -o !temp_folder!\ffmpeg.zip
+    echo [INFO] Finished downloading ffmpeg.exe
+    if exist !temp_folder!\ffmpeg.zip (
+        title Extracting ffmpeg.exe ^| Audio and Video Merger 
+        echo [INFO] Extracting ffmpeg.exe
+    ) else (
+        echo [ERROR] Download failed, please check your network connection
+        echo [ERROR] Download failed, please check your network connection >> error.log
+        exit
+    )
+    mkdir !temp_folder!\unzip >nul 2>nul
+    tar -xf !temp_folder!\ffmpeg.zip -C !temp_folder!\unzip >nul 2>nul
+    for /d %%i in (!temp_folder!\unzip\*) do set SUB_FOLDER=%%i
+    if exist "!SUB_FOLDER!\bin\ffmpeg.exe" (
+        move "!SUB_FOLDER!\bin\ffmpeg.exe" ".\ffmpeg.exe" >nul 2>nul
+    ) else (
+        echo [ERROR] ffmpeg.exe not found.
+        echo [ERROR] ffmpeg.exe not found. >> error.log
+        exit
+    )
+    rmdir !temp_folder! /s /q >nul 2>nul
+    for /f "delims=" %%i in ('ffmpeg -version 2^>^&1') do (
+        set "ffmpeg_version=%%i"
+        goto :ddone
+    )
+    :ddone
+    for /f "tokens=2,3" %%a in ("!ffmpeg_version!") do (
+        set "extracted_text=%%a %%b"
+    )
+    echo [INFO] ffmpeg.exe extracted successfully
+    echo [INFO] ffmpeg.exe extracted successfully >> log.log
+    echo [INFO] Dependence ffmpeg %extracted_text%
+    echo [INFO] Dependence ffmpeg %extracted_text% >> log.log
+    echo.
+)
